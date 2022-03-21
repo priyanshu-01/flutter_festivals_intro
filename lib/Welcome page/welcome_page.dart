b140dd0bbd9cdc6437c3f498ec33dfb164b20e2d
Priyanshu Agarwal <priyanshu.agarwal88@gmail.com>
2022-03-22 02:08:14 +0530

    code cleanup

--- lib/Welcome page/welcome_page.dart
+++ lib/Welcome page/welcome_page.dart
@@ -1,50 +1,38 @@
 import 'package:flutter/material.dart';
 
 class WelocomePage extends StatefulWidget {
-  const WelocomePage({Key? key}) : super(key: key);
-
   @override
   State<WelocomePage> createState() => _WelocomePageState();
 }
 
 class _WelocomePageState extends State<WelocomePage> {
-  Color _color = Colors.orange;
-  double _radius = 0;
-  double _dimension = 200;
+  Color _color = Colors.pink;
+  double _height = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          AnimatedContainer(
-            duration: Duration(seconds: 1),
-            height: _dimension,
-            width: _dimension,
-            decoration: BoxDecoration(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: Duration(seconds: 2),
               color: _color,
-              // shape: _boxShape,
-              borderRadius: BorderRadius.circular(_radius),
+              height: _height,
+              width: 100,
             ),
-          ),
-          TextButton(
-            child: Text(
-              "change color",
-              style: TextStyle(color: Colors.grey[800]),
-            ),
-            onPressed: () {
-              print("color changes to pink");
-              setState(() {
-                _color = _color == Colors.pink ? Colors.orange : Colors.pink;
-                _radius = _radius == 0 ? _dimension : 0;
-                _dimension = _dimension == 200 ? 80 : 200;
-              });
-              // action
-            },
-          ),
-        ],
-      )),
+            TextButton(
+              onPressed: () {
+                _height = 200;
+                _color = Colors.orange;
+                setState(() {});
+              },
+              child: Text("Change color"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
